e809c5343fad610356a91e8652d120302b294427
CarPaz <carla.astroza@usach.cl>
2025-05-20 16:42:34 -0400

    Tabla producto y lista de eseos

--- Entrega3SQL.sql
+++ Entrega3SQL.sql
@@ -1,15 +1,33 @@
-create table if not exists usuario
-(
-id_usuario SERIAL PRIMARY KEY,
-nombre_user VARCHAR(16) NOT NULL,
-correo_user CHAR(30) NOT NULL,
-clave VARCHAR(8) NOT NULL,
-jefe_tienda BOOLEAN NOT NULL,
-cliente BOOLEAN NOT NULL,
-calle VARCHAR(20),
-nro_depto INT,
-nro_calle INT,
-region VARCHAR(30),
-ciudad VARCHAR(30)
+CREATE TABLE IF NOT EXISTS usuario (
+  id_usuario SERIAL PRIMARY KEY,
+  nombre_user VARCHAR(16) NOT NULL,
+  correo_user VARCHAR(30) NOT NULL,
+  clave VARCHAR(8) NOT NULL,
+  jefe_tienda BOOLEAN NOT NULL,
+  cliente BOOLEAN NOT NULL,
+  calle VARCHAR(20),
+  nro_depto INT,
+  nro_calle INT,
+  region VARCHAR(30),
+  ciudad VARCHAR(30)
+);
 
+CREATE TABLE IF NOT EXISTS producto (
+  id_producto SERIAL PRIMARY KEY, 
+  nombre_producto VARCHAR(30),
+  precio INT, 
+  url_imagen VARCHAR(100),
+  descripcion VARCHAR(500),
+  juegos_mesa BOOLEAN, 
+  juegos_carta BOOLEAN, 
+  stock INT
+);
+
+CREATE TABLE IF NOT EXISTS lista_deseos (
+  id_lista SERIAL PRIMARY KEY,
+  id_usuario INT NOT NULL,
+  id_producto INT NOT NULL,
+  total_lista INT,
+  FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario),
+  FOREIGN KEY (id_producto) REFERENCES producto(id_producto)
 );
