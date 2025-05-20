7e788fded1765e6ec2845b3f68b3cfe506643b80
CarPaz <carla.astroza@usach.cl>
2025-05-19 21:16:23 -0400

    Tabla usuario

--- Entrega3SQL.sql
+++ Entrega3SQL.sql
@@ -1,5 +1,14 @@
 create table if not exists usuario
-{
-codigo serial PRIMARY KEY
-
-}
+(
+id_usuario SERIAL PRIMARY KEY,
+nombre_user VARCHAR(16) NOT NULL,
+correo_user CHAR(30) NOT NULL,
+clave VARCHAR(8) NOT NULL,
+jefe_tienda BOOLEAN NOT NULL,
+cliente BOOLEAN NOT NULL,
+calle VARCHAR(20),
+nro_depto INT,
+nro_calle INT,
+region VARCHAR(30),
+ciudad VARCHAR(30)
+);
